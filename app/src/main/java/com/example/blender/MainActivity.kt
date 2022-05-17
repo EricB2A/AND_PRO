d56a0b0dc5171d5daa7fce41be0a62c678dbb8d7
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2022-05-17 16:57:28 +0200

    BLE initial

--- app/src/main/java/com/example/blender/MainActivity.kt
+++ app/src/main/java/com/example/blender/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.blender
 
-import androidx.appcompat.app.AppCompatActivity
+import android.bluetooth.le.ScanResult
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.welie.blessed.BluetoothCentralManager
+import com.welie.blessed.BluetoothCentralManagerCallback
+import com.welie.blessed.BluetoothPeripheral
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var central : BluetoothCentralManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        central = BluetoothCentralManager(
+            applicationContext,
+            bluetoothCentralManagerCallback,
+            Handler(Looper.getMainLooper())
+        )
+
+        central.scanForPeripherals()
     }
+
+    private val bluetoothCentralManagerCallback: BluetoothCentralManagerCallback =
+        object : BluetoothCentralManagerCallback() {
+            override fun onDiscoveredPeripheral(
+                peripheral: BluetoothPeripheral,
+                scanResult: ScanResult
+            ) {
+                Log.d("test", peripheral.name)
+                //central.stopScan()
+                //central.connectPeripheral(peripheral, peripheralCallback)
+            }
+        }
 }
