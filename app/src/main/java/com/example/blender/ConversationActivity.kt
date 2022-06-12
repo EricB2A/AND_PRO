3a11b153ef095d94521668a0130f566bdaf76ef5
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2022-06-12 11:25:35 +0200

    auto scrolling to bottom for each new message in discussion

--- app/src/main/java/com/example/blender/ConversationActivity.kt
+++ app/src/main/java/com/example/blender/ConversationActivity.kt
@@ -18,6 +18,7 @@ class ConversationActivity : AppCompatActivity() {
     private val conversationAdapter = ConversationRecyclerAdapter()
     private lateinit var btnSend: Button
     private lateinit var userInput: EditText
+    private lateinit var recyclerView : RecyclerView
 
     private val conversationViewModel: ConversationViewModel by viewModels {
         ConversationViewModelFactory((application as Blender).repository)
@@ -29,6 +30,7 @@ class ConversationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_conversation)
         btnSend = findViewById(R.id.button_send)
         userInput = findViewById(R.id.editText_userInput)
+        recyclerView = findViewById(R.id.recylerView_conversation)
         initRecycler()
 
 
@@ -42,6 +44,7 @@ class ConversationActivity : AppCompatActivity() {
         conversationViewModel.getConversationMessage(convId)
             .observe(this) { value ->
                 conversationAdapter.items = value.messages!!
+                recyclerView.scrollToPosition(conversationAdapter.itemCount - 1)
             }
 
 
@@ -55,6 +58,8 @@ class ConversationActivity : AppCompatActivity() {
                 Calendar.getInstance(),
                 MessageType.SENT
             )
+
+
 
             BLEClient.getInstance(null).sendMessage(uuid, msg)
 
