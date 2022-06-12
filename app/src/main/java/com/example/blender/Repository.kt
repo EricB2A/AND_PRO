5b5be948892856312cd113ee680993384849a106
Ilias <igoujgali@gmail.com>
2022-06-12 15:44:38 +0200

    commentaire repository

--- app/src/main/java/com/example/blender/Repository.kt
+++ app/src/main/java/com/example/blender/Repository.kt
@@ -1,43 +1,64 @@
 package com.example.blender
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import com.example.blender.dao.ConversationDao
 import com.example.blender.dao.MessageDao
 import com.example.blender.dao.ProfileDao
 import com.example.blender.models.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.*
 import java.util.stream.IntStream.range
 
+/**
+ * Repository contenant les DAOs pour les accès à la db
+ */
 class Repository(
     private val conversationDao: ConversationDao,
     private val messageDao: MessageDao,
     private val profileDao: ProfileDao,
     private val scope: CoroutineScope
 ) {
-    fun getAllConversationWithMessage() : LiveData<List<ConversationMessage>> {
+    /**
+     * Retourne toutes les conversations et leurs messages
+     */
+    fun getAllConversationWithMessage(): LiveData<List<ConversationMessage>> {
         return conversationDao.getConversationWithMessage()
     }
 
+    /**
+     * Retourne le profile de l'utilsiateur de l'application
+     */
     fun getMyProfile(): LiveData<Profile?> {
         return profileDao.getMyProfile()
     }
 
+    /**
+     * Retourne le profile de l'utilisateur  (version coroutine)
+     */
     private suspend fun getProfileByUUID(uuid: String): Profile? {
         return profileDao.getByUUID(uuid)
     }
 
-
-
+    /**
+     * Ajoute un profile d'un utilisateur distant (version coroutine)
+     */
     suspend fun addRemoteProfile(remoteProfile: Profile) {
         val p = getProfileByUUID(remoteProfile.uuid)
         if (p == null) {
-            Log.d("test", "null")
             val newP = remoteProfile.toRemoteProfile()
             scope.launch {
                 val id = profileDao.insert(newP)
-                insertConversation(Conversation(null, remoteProfile.pseudo, Calendar.getInstance(), id, remoteProfile.uuid))
+                insertConversation(
+                    Conversation(
+                        null,
+                        remoteProfile.pseudo,
+                        Calendar.getInstance(),
+                        id,
+                        remoteProfile.uuid
+                    )
+                )
             }
         } else {
             scope.launch {
@@ -53,28 +74,43 @@ class Repository(
         }
     }
 
+    /**
+     * Insère un profile et retourne l'id de l'élément inséré
+     */
     fun insertProfile(profile: Profile): Long {
-        var id : Long = 0
+        var id: Long = 0
         scope.launch(Dispatchers.IO) {
             id = profileDao.insert(profile)
         }
         return id
     }
 
+    /**
+     * Met à jour le profile dans la db
+     */
     fun updateProfile(profile: Profile) {
         scope.launch(Dispatchers.IO) {
             profileDao.update(profile)
         }
     }
 
+    /**
+     * Insère la conversation dans la db
+     */
     fun insertConversation(conversation: Conversation) {
         conversationDao.insert(conversation)
     }
 
+    /**
+     * Recupère la conversation et ses messages depuis la db en fonction de l'id donné
+     */
     fun getConversationMessage(id: Long): LiveData<ConversationMessage> {
         return conversationDao.getById(id)
     }
 
+    /**
+     * Insère la conversation et ses messages dans la db
+     */
     fun insertConversationMessages(conversation: Conversation, messages: List<Message>?) {
         scope.launch(Dispatchers.IO) {
             val nid = conversationDao.insert(conversation)
@@ -87,6 +123,9 @@ class Repository(
         }
     }
 
+    /**
+     * Insère un nouveau message dans la db
+     */
     fun insertMessage(message: Message) {
         scope.launch(Dispatchers.IO) {
             messageDao.insert(message)
@@ -94,14 +133,14 @@ class Repository(
         }
     }
 
+    /**
+     * Insère un message dans la db. Cette fonction est utilisé pour insérer les messages
+     * depuis BLE, les messages insérés proviennet donc des autres utilisateurs
+     */
     fun insertReceivedMessage(messageWithUUID: MessageWithProfileUUID) {
         scope.launch(Dispatchers.IO) {
             val uuid = messageWithUUID.uuid
             val message = messageWithUUID.message
-            Log.d("TEST MESSAGES", uuid)
-            Log.d("TEST MESSAGES", message.content)
-            Log.d("TEST MESSAGES", profileDao.getConvIdFromUUID(uuid).toString())
-            Log.d("TEST MESSAGES", profileDao.getConvIdFromUUID(uuid)?.conversation?.id.toString())
             message.type = MessageType.RECEIVED
             message.id = null
             message.convId = profileDao.getConvIdFromUUID(uuid)?.conversation?.id!!
@@ -110,6 +149,9 @@ class Repository(
         }
     }
 
+    /**
+     * Permet de reset la db
+     */
     fun reset() {
         scope.launch(Dispatchers.IO) {
             conversationDao.deleteAllConversations()
@@ -119,10 +161,17 @@ class Repository(
 
     }
 
+    /**
+     * Retourne le nobmre de message reçu pour une conversation donnée
+     * ( est utilisé pour afficher le profile de l'utilsiateur de manière progressive)
+     */
     fun getNbReceivedMessage(convId: Long): LiveData<Int> {
         return messageDao.getByConvId(convId)
     }
 
+    /**
+     * Retourne le profile en fonction de l'UUID de l'utilisateur fourni en paramètre
+     */
     fun getLiveProfileByUUID(uuid: String): LiveData<Profile> {
         return profileDao.getLiveByUUID(uuid)
     }
